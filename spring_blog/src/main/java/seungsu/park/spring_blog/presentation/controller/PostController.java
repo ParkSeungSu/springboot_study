7e091f25f1c35285e8c0d07beaf1eea8b8178e83
ParkSeungSu <cldkr0401@naver.com>
2021-07-04 11:38:52 +0900

    .

--- spring_blog/src/main/java/seungsu/park/spring_blog/presentation/controller/PostController.java
+++ spring_blog/src/main/java/seungsu/park/spring_blog/presentation/controller/PostController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +27,10 @@ public class PostController {
         return "form";
     }
     @RequestMapping(value = "/write",method = RequestMethod.POST)
-    public String write(Post post){
+    public String write(@Valid Post post,BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "form";
+        }
         post.setRegDate(new Date());
         return "redirect:/post/"+postDao.save(post).getId();
     }
