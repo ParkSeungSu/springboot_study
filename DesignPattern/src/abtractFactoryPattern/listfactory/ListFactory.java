3a7d2f9b49cfd11198fa95c4c7a54bf313c1e00a
ParkSeungSu <cldkr0401@naver.com>
2021-07-31 17:28:33 +0900

    abstarctFactoryPattern

---
+++ DesignPattern/src/abtractFactoryPattern/listfactory/ListFactory.java
@@ -0,0 +1,26 @@
+package abtractFactoryPattern.listfactory;
+
+import abtractFactoryPattern.factory.Factory;
+import abtractFactoryPattern.factory.Link;
+import abtractFactoryPattern.factory.Page;
+import abtractFactoryPattern.factory.Tray;
+
+public class ListFactory extends Factory {
+
+	@Override
+	public Link createLink(String caption, String url) {
+		return new ListLink(caption,url);
+	}
+
+	@Override
+	public Tray createTray(String caption) {
+		return new ListTray(caption);
+	}
+
+	@Override
+	public Page createPage(String title, String author) {
+	
+		return new ListPage(title, author);
+	}
+
+}
---
+++ DesignPattern/src/abtractFactoryPattern/listfactory/ListTray.java
@@ -0,0 +1,27 @@
+package abtractFactoryPattern.listfactory;
+
+import abtractFactoryPattern.factory.Item;
+import abtractFactoryPattern.factory.Tray;
+
+public class ListTray extends Tray {
+
+	public ListTray(String caption) {
+		super(caption);
+	}
+
+	@Override
+	public String makeHTML() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<li>\n");
+		buffer.append(caption + "\n");
+		buffer.append("<ul>\n");
+		for (Object o : tray) {
+			Item item = (Item) o;
+			buffer.append(item.makeHTML());
+		}
+		buffer.append("</ul>\n");
+		buffer.append("</li>\n");
+		return buffer.toString();
+	}
+
+}
